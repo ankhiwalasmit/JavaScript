fa31c78260a8402ea0346405ec772d24d66e7f46
ankhiwalasmit <ankhiwalasmit@gmail.com>
2024-06-08 11:23:45 +0530

    conversion of datatypes

---
+++ 01_basics/conversionOperations.js
@@ -0,0 +1,34 @@
+// let score = "33"
+// let score = "33abc"
+// let score = null
+// let score = undefined
+// let score = true
+let score = "smit"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber); // NaN(Not a Number)
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+// let isLoggedIn = 0
+// let isLoggedIn = 1
+// let isLoggedIn = ""
+let isLoggedIn = "smit"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn); 
+
+// 1 => true; 0 => false
+// "smit" => true; "" => false
+// null => false; undefined => false
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);

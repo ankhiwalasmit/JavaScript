206cbf0f950ce37d054f0e76ff153bde4c445883
ankhiwalasmit <ankhiwalasmit@gmail.com>
2024-06-08 09:00:15 +0530

    datatypes

---
+++ 01_basics/dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all js code as newer version
+
+// alert("hello") // we are using nodejs, not browser
+
+// code readability should be high
+ 
+let name = "smit" // string
+let age = 25 // number
+let isLoggedIn = true // boolean
+let temp = null // null
+let state; // undefined
+
+// Data Types: 
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value, representation of empty value
+// undefined => value is not yet assigned, variable is defined but value is not assigned to it 
+// symbol => unique
+
+// object
+
+console.log(typeof "smit");
+console.log(typeof age);
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined

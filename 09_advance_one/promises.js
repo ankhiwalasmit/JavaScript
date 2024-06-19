364962ac1263ca73aedc00a474c6cb8cd014140f
ankhiwalasmit <ankhiwalasmit@gmail.com>
2024-06-19 15:45:11 +0530

    promises

--- 09_advance_one/promises.js
+++ 09_advance_one/promises.js
@@ -19,86 +19,79 @@ new Promise(function(resolve, reject){
     console.log("Async 2 resolved");
 })
 
-// const promiseThree = new Promise(function(resolve, reject){
-//     setTimeout(function(){
-//         resolve({username: "Smit", email: "smit@google.com"})
-//     }, 1000);
-// })
+const promiseThree = new Promise(function(resolve, reject){
+    setTimeout(function(){
+        resolve({username: "Smit", email: "smit@google.com"})
+    }, 1000);
+})
 
-// promiseThree.then(function(user){
-//     console.log(user);
-// })
+promiseThree.then(function(user){
+    console.log(user);
+})
 
-// const promiseFour = new Promise(function(resolve, reject){
-//     setTimeout(function(){
-//         let error = true
-//         if (!error) {
-//             resolve({username: "smit", password: "123"})
-//         } else {
-//             reject('ERROR: Something went wrong')
-//         }
-//     }, 1000)
-// })
+const promiseFour = new Promise(function(resolve, reject){
+    setTimeout(function(){
+        let error = true
+        // let error = false
+        if (!error) {
+            resolve({username: "smit", password: "123"})
+        } else {
+            reject('ERROR: Something went wrong')
+        }
+    }, 1000)
+})
 
-// promiseFour
-// .then((user) => { // .then runs(executed) when the promise is resolved
-//     console.log(user)
-//     return user.username
-// })
-// .then((username) => { // return value of the first then method is used in second then method and so on. this is known as promise chaining 
-//     console.log(username);
-// })
-// .catch(function(error){ // .catch runs(executed) when the promise is rejected
-//     console.log(error);
-// })
-// .finally(() => console.log("The promise is either resolved or rejected")) // .finally always runs(executed) whether the promise is resolved or rejected
+promiseFour
+.then((user) => { // .then runs(executed) when the promise is resolved
+    console.log(user)
+    return user.username
+})
+.then((username) => { // return value of the first then method is used in second then method and so on. this is known as promise chaining 
+    console.log(username);
+})
+.catch(function(error){ // .catch runs(executed) when the promise is rejected
+    console.log(error);
+})
+.finally(() => console.log("The promise is either resolved or rejected")) // .finally always runs(executed) whether the promise is resolved or rejected
 
-// const promiseFive = new Promise(function(resolve, reject){
-//     setTimeout(function(){
-//         let error = true
-//         if (!error) {
-//             resolve({username: "javascript", password: "123"})
-//         } else {
-//             reject('ERROR: JS went wrong')
-//         }
-//     }, 1000)
-// })
+const promiseFive = new Promise(function(resolve, reject){
+    setTimeout(function(){
+        let error = true
+        // let error = false
+        if (!error) {
+            resolve({username: "javascript", password: "123"})
+        } else {
+            reject('ERROR: JS went wrong')
+        }
+    }, 1000)
+})
 
-// async function consumePromiseFive(){
-//     // const response = await promiseFive
-//     // console.log(response);
+async function consumePromiseFive(){
+    // const response = await promiseFive
+    // console.log(response);
+    try{
+        const response = await promiseFive
+        console.log(response);
+    } catch(error){
+        console.log(error);
+    }
+}
+
+consumePromiseFive()
+
+// async function getAllUsers(){
 //     try{
-//         const response = await promiseFive
-//         console.log(response);
+//         const response = await fetch('https://jsonplaceholder.typicode.com/users')
+//         const data = await response.json()
+//         console.log(data);
 //     } catch(error){
-//         console.log(error);
+//         console.log("E: ", error);
 //     }
 // }
 
-// consumePromiseFive()
+// getAllUsers()
 
-// // async function getAllUsers(){
-// //     try{
-// //         const response = await fetch('https://jsonplaceholder.typicode.com/users')
-// //         const data = await response.json()
-// //         console.log(data);
-// //     } catch(error){
-// //         console.log("E: ", error);
-// //     }
-// // }
-
-// // getAllUsers()
-
-// // fetch('https://jsonplaceholder.typicode.com/users')
-// // .then((response) => {
-// //     return response.json()
-// // })
-// // .then((data) => {
-// //     console.log(data)
-// // })
-// // .catch((error) => console.log(error))
-
-// fetch('https://api.github.com/users/ankhiwalasmit')
+// fetch('https://jsonplaceholder.typicode.com/users')
 // .then((response) => {
 //     return response.json()
 // })
@@ -106,3 +99,12 @@ new Promise(function(resolve, reject){
 //     console.log(data)
 // })
 // .catch((error) => console.log(error))
+
+fetch('https://api.github.com/users/ankhiwalasmit')
+.then((response) => {
+    return response.json()
+})
+.then((data) => {
+    console.log(data)
+})
+.catch((error) => console.log(error))
